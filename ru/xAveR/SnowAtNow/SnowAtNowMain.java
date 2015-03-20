68414131899ed47453c0e41b9718bd037153d2a6
xAveR <xxxaverrr@gmail.com>
2015-03-20 19:31:45 +0300

    SnowAtNow
    v4.0.0
    Support WG! YAY

--- ru/xAveR/SnowAtNow/SnowAtNowMain.java
+++ ru/xAveR/SnowAtNow/SnowAtNowMain.java
@@ -2,7 +2,6 @@ package ru.xAveR.SnowAtNow;
 
 
 import org.bukkit.Material;
-import org.bukkit.Server;
 import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -20,11 +19,17 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.BlockIterator;
 
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+
 public class SnowAtNowMain extends JavaPlugin implements Listener{
+    WorldGuardPlugin wg;
 	@Override
-  public void onEnable()
-  {
+    public void onEnable()
+    {
     getServer().getPluginManager().registerEvents(this, this);
+    
+    wg = (WorldGuardPlugin)getServer().getPluginManager().getPlugin("WorldGuard");
+    
     ItemStack ice = new ItemStack(Material.ICE);
     ShapedRecipe rIce = new ShapedRecipe(ice);
     rIce.shape(new String[] { "SSS", "SBS", "SSS" });
@@ -49,12 +54,17 @@ public class SnowAtNowMain extends JavaPlugin implements Listener{
   }
 @EventHandler
   public void ProjectileHitEvent(ProjectileHitEvent e){
+	
 	  Entity s = e.getEntity();
+	  Player sh = (Player) e.getEntity().getShooter();
 	  if(s instanceof Snowball){
 		  BlockIterator i = new BlockIterator(e.getEntity().getWorld(), e.getEntity().getLocation().toVector(), e.getEntity().getVelocity().normalize(), 0.0D, 4);
 		   Block hB = null;
 		  while(i.hasNext()){
 			  hB = i.next();
+			  if(!(wg.canBuild(sh, hB))){
+			  	s.remove();
+			  } else {
 			  if(hB.getType() != Material.AIR){
 				  if(hB.getRelative(BlockFace.UP).getType() != Material.AIR || hB.getType() == Material.SNOW){
 					  break;
@@ -68,14 +78,15 @@ public class SnowAtNowMain extends JavaPlugin implements Listener{
 		  }
 		  if(hB.getType() == Material.WATER){
 			  hB.setType(Material.ICE);
+		  		}
 		  	}
-		  }
+		  	} 
 	  } 
 	@EventHandler
 	public void SnowFormEvent(EntityBlockFormEvent e){
 		final Block b = e.getBlock();
 		Entity en = e.getEntity();
-		if(en instanceof Player || en instanceof Snowman|| en instanceof Server){
+		if(en instanceof Snowman){
 		if(b.getType() == Material.SNOW || b.getType() == Material.ICE){
 			if(b.getBiome() != Biome.TAIGA ||b.getBiome() != Biome.TAIGA_HILLS||b.getBiome() != Biome.ICE_MOUNTAINS||b.getBiome() != Biome.ICE_PLAINS||b.getBiome() != Biome.FROZEN_OCEAN||b.getBiome() != Biome.FROZEN_RIVER){
 				e.setCancelled(true);
@@ -83,4 +94,5 @@ public class SnowAtNowMain extends JavaPlugin implements Listener{
 			}
 		}
 	}
+
 }
